9f0c7798d97f8964bd82eb689ed66608616f0f0b
JohnnieDom <johnniedom1@gmail.com>
2022-12-17 23:06:54 +0100

    Day 17 New project

--- script.js
+++ script.js
@@ -200,7 +200,7 @@ is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy
 🐶
 ")
 4. Run the function for both test datasets
-Test data:
+Test data:nwe
 § Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
 § Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 Hints: Use tools from all lectures in this section so far 😉
